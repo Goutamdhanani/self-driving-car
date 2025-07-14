5685dfef8d15cef19238a0beab31bdc1d26130af
goutam kumar <106019412+Goutamdhanani@users.noreply.github.com>
2025-07-14 22:21:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
-// Reduced number of cars for better performance
-const N=20;
+// Reduce the number of cars for better performance
+const N=30;
 const cars=generateCars(N);
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
@@ -17,13 +17,13 @@ if(localStorage.getItem("bestBrain")){
         cars[i].brain=JSON.parse(
             localStorage.getItem("bestBrain"));
         if(i!=0){
-            // Reduced mutation rate from 0.1 to 0.05 for more stable behavior
+            // Reduce mutation rate for more stable behavior
             NeuralNetwork.mutate(cars[i].brain,0.05);
         }
     }
 }
 
-// Modified traffic with more space between cars
+// Increase spacing between traffic cars
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2,getRandomColor()),
     new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2,getRandomColor()),
