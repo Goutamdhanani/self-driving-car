f4530bf76b85c6e045a6bd19a5ebb08b3948e532
goutam kumar <106019412+Goutamdhanani@users.noreply.github.com>
2025-07-14 22:13:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
-const N=1;
+const N=100;
 const cars=generateCars(N);
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
